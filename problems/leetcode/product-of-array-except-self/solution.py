11dcd6ac5611587499e715b60d7dc392b783895e
ironsidez <ironsides1231@gmail.com>
2025-11-09 18:24:40 -0500

    add solutions for product of array except self and top k frequent elements

--- problems/leetcode/product-of-array-except-self/solution.py
+++ problems/leetcode/product-of-array-except-self/solution.py
@@ -2,5 +2,13 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # TODO: Implement solution
-        pass
+        preproduct = 1
+        postproduct =1
+        result = [1] * len(nums)
+        for i in range(len(nums)):
+            result[i] *= preproduct  
+            preproduct *= nums[i]
+            result[-1 - i] *= postproduct
+            postproduct *= nums[-1 - i]
+        
+        return result
--- problems/leetcode/top-k-frequent-elements/solution.py
+++ problems/leetcode/top-k-frequent-elements/solution.py
@@ -2,5 +2,15 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # TODO: Implement solution
-        pass
+        frequencyMap = {}
+        for number in nums:
+            if number in frequencyMap:
+                frequencyMap[number] += 1
+            else:
+                frequencyMap[number] = 1
+
+        sortedMap = sorted(frequencyMap.items(), key=lambda x: x[1], reverse=True)
+        topK = sortedMap[:k]
+        return [item[0] for item in topK]
+            
+
