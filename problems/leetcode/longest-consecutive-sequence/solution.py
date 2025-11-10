10776da2f56457a6d4798c5a1ec06ab504b52d03
ironsidez <ironsides1231@gmail.com>
2025-11-09 23:46:38 -0500

    Add solution for longest consecutive sequence

--- problems/leetcode/longest-consecutive-sequence/solution.py
+++ problems/leetcode/longest-consecutive-sequence/solution.py
@@ -2,5 +2,18 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # TODO: Implement solution
-        pass
+        sortedNums = sorted(nums)
+        previousNum = sortedNums[0] if sortedNums else None
+        longestSequenceLen = 1
+        currentSequenceLen = 1
+
+        for num in sortedNums:
+            if num == previousNum + 1:
+                currentSequenceLen += 1
+                if currentSequenceLen > longestSequenceLen:
+                    longestSequenceLen = currentSequenceLen
+            elif num != previousNum:
+                currentSequenceLen = 1
+                    
+            previousNum = num
+        return longestSequenceLen
--- problems/leetcode/longest-consecutive-sequence/test_solution.py
+++ problems/leetcode/longest-consecutive-sequence/test_solution.py
@@ -5,7 +5,7 @@ from typing import List
 
 # Add the current directory to the path to import solution
 sys.path.insert(0, os.path.dirname(__file__))
-from solution import Solution
+from .solution import Solution
 
 class TestLongestConsecutive(unittest.TestCase):
     def setUp(self):
