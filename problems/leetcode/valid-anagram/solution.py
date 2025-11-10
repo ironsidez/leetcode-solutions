0129576630ea1048989666bd3228fb44bb473de3
ironsidez <ironsides1231@gmail.com>
2025-11-10 15:59:29 -0500

    Add solution for valid anagram

--- problems/leetcode/valid-anagram/solution.py
+++ problems/leetcode/valid-anagram/solution.py
@@ -1,6 +1,7 @@
-
+from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # TODO: Implement solution
-        pass
+        if len(s) != len(t):
+            return False
+        return Counter(s) == Counter(t)
--- problems/leetcode/valid-anagram/test_solution.py
+++ problems/leetcode/valid-anagram/test_solution.py
@@ -5,21 +5,19 @@ import os
 
 # Add the current directory to the path to import solution
 sys.path.insert(0, os.path.dirname(__file__))
-from solution import Solution
+from .solution import Solution
 
 class TestValidAnagram(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
     
     def test_example_1(self):
-        # s = "anagram", t = "nagaram"
-        # Expected: True
-        pass
+        s = "anagram", t = "nagaram"
+        self.assertTrue(self.solution.isAnagram(s, t))
     
     def test_example_2(self):
-        # s = "rat", t = "car"
-        # Expected: False
-        pass
+        s = "rat", t = "car"
+        self.assertFalse(self.solution.isAnagram(s, t))
 
 if __name__ == '__main__':
     unittest.main()
